5f2991b941ed1b517c3a30a9620f0d17e85b6089
Jan de Jongen <jandejongen4@gmail.com>
2026-03-16 16:24:17 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+import cv2
+import mediapipe as mp
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=2,
+    min_detection_confidence=0.5
+)
+mp_draw = mp.solutions.drawing_utils
+
+class CamApp(App):
+    def build(self):
+        self.img1 = Image()
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update, 1.0/30.0)
+        return self.img1
+
+    def update(self, dt):
+        ret, frame = self.capture.read()
+        if ret:
+            frame = cv2.flip(frame, 1)
+            imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = hands.process(imgRGB)
+
+            if results.multi_hand_landmarks:
+                for handLms in results.multi_hand_landmarks:
+                    mp_draw.draw_landmarks(frame, handLms, mp_hands.HAND_CONNECTIONS)
+
+            buf = cv2.flip(frame, 0).tobytes()
+            texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            self.img1.texture = texture
+
+    def on_stop(self):
+        self.capture.release()
+
+if __name__ == '__main__':
+    CamApp().run()
